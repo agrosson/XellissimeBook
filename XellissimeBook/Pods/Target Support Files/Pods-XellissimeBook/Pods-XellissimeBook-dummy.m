0dfa73676e28387e2c9184b3075f31a6e8e0fc21
agrosson <agrosson@gmail.com>
2019-04-30 21:49:39 +0200

    Add FireBase

---
+++ XellissimeBook/Pods/Target Support Files/Pods-XellissimeBook/Pods-XellissimeBook-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XellissimeBook : NSObject
+@end
+@implementation PodsDummy_Pods_XellissimeBook
+@end
---
+++ XellissimeBook/Pods/Target Support Files/Pods-XellissimeBookTests/Pods-XellissimeBookTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XellissimeBookTests : NSObject
+@end
+@implementation PodsDummy_Pods_XellissimeBookTests
+@end
