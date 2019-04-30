a9717646f32b83e354ff4e1772f62616911a290d
agrosson <agrosson@gmail.com>
2019-04-30 15:43:39 +0200

    update design

--- XellissimeBook/XellissimeBook/Controller/InitialScreenViewController.swift
+++ XellissimeBook/XellissimeBook/Controller/InitialScreenViewController.swift
@@ -15,15 +15,25 @@ class InitialScreenViewController: UIViewController {
     // MARK: - Outlets - Views
     @IBOutlet var initialPopoverView: UIView!
     @IBOutlet var noAccessPopoverView: UIView!
+    @IBOutlet weak var logStackView: UIStackView!
     // MARK: - Outlets - Labels
     @IBOutlet weak var popoverLabel: UILabel!
-    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var alreadyAccountLabel: UILabel!
+    // MARK: - Outlets - TextFields
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
     // MARK: - Actions
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var logInButton: UIButton!
     @IBAction func iAcceptButtonPressed(_ sender: UIButton) {
         SettingsService.hasAcceptedConditions = true
         self.initialPopoverView.removeFromSuperview()
-        mainLabel.isHidden = false
+       outletDisplay(shown: true)
     }
+    
     @IBAction func iRefuseButtonPressed(_ sender: UIButton) {
         print("User has pressed refused button")
         if popoverLabel.text == TextAndString.shared.initialWarning {
@@ -43,14 +53,15 @@ class InitialScreenViewController: UIViewController {
     // MARK: -
     override func viewDidLoad() {
         super.viewDidLoad()
-        mainLabel.isHidden = true
+        setUpPageLogIn()
+        outletDisplay(shown: false)
         print("on load: est ce que le user a deja été connecté?")
         print(SettingsService.hasBeenAlreadyConnected)
         print("on check")
         checkIfHasBeenAlreadyConnected(SettingsService.hasBeenAlreadyConnected)
         print("on change le status")
         SettingsService.hasBeenAlreadyConnected = true
-        
+       
     }
 
 
@@ -81,13 +92,14 @@ extension InitialScreenViewController {
      */
     private func checkIfConditionsAccepted(_ accepted: Bool){
         if !accepted {
+         //   outletDisplay(shown: false)
             self.view.addSubview(initialPopoverView)
             adaptPopoverSize()
             initialPopoverView.backgroundColor = .orange
             popoverLabel.text = TextAndString.shared.conditionMustBeAccepted
         } else {
-            mainLabel.isHidden = false
             print("conditions already accepted")
+         outletDisplay(shown: true)
         }
     }
 }
@@ -104,6 +116,46 @@ extension InitialScreenViewController {
         noAccessPopoverView.bounds.size.width = 2 * screenWidth/3
         noAccessPopoverView.layer.cornerRadius = popoverRoundRadius
         self.noAccessPopoverView.center = self.noAccessPopoverView.superview!.center
+    }
+}
+
+extension InitialScreenViewController {
+    private func setUpPageLogIn(){
+        userNameTextField.backgroundColor = #colorLiteral(red: 0.9092954993, green: 0.865521729, blue: 0.8485594392, alpha: 1)
+        userNameTextField.textColor = #colorLiteral(red: 0.5201328993, green: 0.5498541594, blue: 0.5580087304, alpha: 1)
+        passwordTextField.backgroundColor = #colorLiteral(red: 0.9092954993, green: 0.865521729, blue: 0.8485594392, alpha: 1)
+        passwordTextField.textColor = #colorLiteral(red: 0.5201328993, green: 0.5498541594, blue: 0.5580087304, alpha: 1)
+        emailTextField.backgroundColor = #colorLiteral(red: 0.9092954993, green: 0.865521729, blue: 0.8485594392, alpha: 1)
+        emailTextField.textColor = #colorLiteral(red: 0.5201328993, green: 0.5498541594, blue: 0.5580087304, alpha: 1)
+        
+        logInButton.layer.cornerRadius = 20
+        logInButton.layer.borderWidth = 3
+        logInButton.layer.borderColor = #colorLiteral(red: 0.9092954993, green: 0.865521729, blue: 0.8485594392, alpha: 1)
+        signUpButton.layer.cornerRadius = 20
+        
+    }
+    
+    private func outletDisplay(shown: Bool){
+        if shown == true {
+            self.logStackView.isHidden = !shown
+            self.signUpButton.isHidden = !shown
+            self.alreadyAccountLabel.isHidden = !shown
+            self.logInButton.isHidden = !shown
+        } else {
+            self.logStackView.isHidden = true
+            self.signUpButton.isHidden = true
+            self.alreadyAccountLabel.isHidden = true
+            self.logInButton.isHidden = true
+        }
+    }
+    
+}
+
+
+
+extension InitialScreenViewController : UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
     }
 }
 
