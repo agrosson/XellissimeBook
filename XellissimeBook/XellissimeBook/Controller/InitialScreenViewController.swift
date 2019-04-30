42529fda8996d6d23b1903d468e052a3d66e429c
agrosson <agrosson@gmail.com>
2019-04-30 10:24:13 +0200

    Add popoverView to check if conditions have been accepted

--- XellissimeBook/XellissimeBook/Controller/InitialScreenViewController.swift
+++ XellissimeBook/XellissimeBook/Controller/InitialScreenViewController.swift
@@ -11,14 +11,27 @@ import UIKit
 class InitialScreenViewController: UIViewController {
 
     // Elements of the initial Popover View
+    
+    // MARK: - Outlets - Views
     @IBOutlet var initialPopoverView: UIView!
+    @IBOutlet var noAccessPopoverView: UIView!
+    // MARK: - Outlets - Labels
     @IBOutlet weak var popoverLabel: UILabel!
+    // MARK: - Actions
     @IBAction func iAcceptButtonPressed(_ sender: UIButton) {
-    }
-    @IBAction func iRefuseButtonPressed(_ sender: UIButton) {
+        SettingsService.hasAcceptedConditions = true
         self.initialPopoverView.removeFromSuperview()
     }
-    
+    @IBAction func iRefuseButtonPressed(_ sender: UIButton) {
+        if popoverLabel.text == TextAndString.shared.initialWarning {
+            popoverLabel.text = TextAndString.shared.conditionMustBeAccepted
+        } else {
+            self.initialPopoverView.removeFromSuperview()
+            self.view.addSubview(noAccessPopoverView)
+            adaptNoAccessPopoverSize()
+        }
+    }
+    // MARK: -
     override func viewDidLoad() {
         super.viewDidLoad()
         print("on load")
@@ -27,12 +40,18 @@ class InitialScreenViewController: UIViewController {
         checkIfHasBeenAlreadyConnected(SettingsService.hasBeenAlreadyConnected)
         print("on change le staus")
         SettingsService.hasBeenAlreadyConnected = true
+        checkIfConditionsAccepted(SettingsService.hasAcceptedConditions)
     }
 
 
 }
-// Here to list all function for viewDidLoad
+    // MARK: - Extension
+// Here to list all functions for viewDidLoad
 extension InitialScreenViewController {
+    /**
+     Function that checks if the user has already been connected
+     - Parameter notFirst : Bool
+     */
     private func checkIfHasBeenAlreadyConnected(_ notFirst: Bool){
         if !notFirst {
             self.view.addSubview(initialPopoverView)
@@ -42,26 +61,34 @@ extension InitialScreenViewController {
             print("warning has been already displayed")
         }
     }
+    /**
+     Function that checks if the user has already accepted conditions
+     - Parameter accepted : Bool
+     */
     private func checkIfConditionsAccepted(_ accepted: Bool){
         if !accepted {
             self.view.addSubview(initialPopoverView)
             adaptPopoverSize()
             popoverLabel.text = TextAndString.shared.conditionMustBeAccepted
         } else {
-            print("condition already accepted")
+            print("conditions already accepted")
         }
     }
 }
 // Here to set up popover display
 extension InitialScreenViewController {
-    // Here to set up popover display
     private func adaptPopoverSize() {
         initialPopoverView.frame.size.height = 2 * screenHeight/3
         initialPopoverView.bounds.size.width = 2 * screenWidth/3
         initialPopoverView.layer.cornerRadius = 30
         self.initialPopoverView.center = self.initialPopoverView.superview!.center
     }
-    
+    private func adaptNoAccessPopoverSize() {
+        noAccessPopoverView.frame.size.height = 8 * screenHeight/10
+        noAccessPopoverView.bounds.size.width = 8 * screenWidth/10
+        noAccessPopoverView.layer.cornerRadius = 30
+        noAccessPopoverView.center = self.noAccessPopoverView.superview!.center
+    }
     
 }
 
